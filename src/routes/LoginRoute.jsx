4e26e46b3215b692a949bcac549bcf78a4d45fdc
L-David-T <davidlez2mgk@gmail.com>
2021-11-05 12:13:37 -0600

    restructurin react router dom V6

--- src/routes/LoginRoute.jsx
+++ src/routes/LoginRoute.jsx
@@ -1,16 +1,25 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LoginScreen from '../components/LoginScreen';
+import {BrowserRouter,Routes,Route } from 'react-router-dom'
+import LoginScreen from '../components/LoginScreen'
 
-// /loginerror
-function LoginRoute() {
+
+
+const LoginRoute = () =>{
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/login" component={LoginScreen} />
-      </Switch>
-    </Router>
-  );
+    <BrowserRouter >
+    <Routes>
+      <Route path="/" element={<LoginScreen/>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
+
+
+
+
+
+
+
+
 
 
 export default LoginRoute;
